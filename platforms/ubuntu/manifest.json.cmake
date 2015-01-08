dc08675f158b5c8ab68d09347cad87f63b545e58
Johnny Jazeix <jazeix@gmail.com>
2015-01-08 23:04:18 +0100

    first version of ubuntu click (not tested)

---
+++ platforms/ubuntu/manifest.json.cmake
@@ -0,0 +1,15 @@
+{
+    "architecture": "any",
+    "description": "gcompris games for children",
+    "framework": "ubuntu-sdk-14.10",
+    "hooks": {
+        "gcompris": {
+            "apparmor": "apparmor.json",
+            "desktop": "gcompris.desktop"
+        }
+    },
+    "maintainer": "Michael Hall <mhall119@ubuntu.com>",
+    "name": "com.ubuntu.developer.mhall119.gcompris",
+    "title": "gcompris",
+    "version": "@GCOMPRIS_VERSION_CODE@"
+}
